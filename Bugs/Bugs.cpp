51ce4012823bbbfecc5bc1c98339886354af7108
Janusz Tomaszewski <mpblit2@gmail.com>
2017-04-29 07:21:20 +0200

    Bugs renamed to BugsSFML, and sfml library tested.

--- Bugs/Bugs.cpp
+++ Bugs/Bugs.cpp
@@ -5,6 +5,91 @@
 #include "Bugs.h"
 #include <boost\any.hpp>
 
+#include <SFML/Graphics.hpp>
+
+
+
+// Let's define some constants for the window.
+
+constexpr unsigned int wndWidth{ 800 }, wndHeight{ 600 };
+
+int main()
+
+{
+
+	// Now we'll create the window.
+
+
+
+	// The class is named `sf::RenderWindow`, and the constructor
+
+	// requires an `sf::Vector2u` (size of the window), and an
+
+	// `std::string` (title of the window).
+
+	sf::RenderWindow window{ { wndWidth, wndHeight }, "Arkanoid - 1" };
+
+
+
+	// Instead of explicitly specifying the `sf::Vector2u` type,
+
+	// we used the {...} uniform initialization syntax.
+
+
+
+	// We'll also set a limit to the framerate, ensuring that the
+
+	// game logic will run at a constant speed.
+
+	window.setFramerateLimit(60);
+
+
+
+	// The next step is "keeping the window alive".
+
+	// This is where the "game loop" comes into play.
+
+	// {Info: game loop}
+
+
+
+	while (true)
+
+	{
+
+		// Every iteration of this loop is a "frame" of our game.
+
+		// We'll begin our frame by clearing the window from previously
+
+		// drawn graphics.
+
+		window.clear(sf::Color::Black);
+
+
+
+		// Then we'll check the input state. In this case, if the
+
+		// player presses the "Escape" key, we'll jump outside of the
+
+		// loop, destroying the window and terminating the program.
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) break;
+
+
+
+		// Show the window's contents.
+
+		window.display();
+
+	}
+
+
+
+	return 0;
+
+}
+
+/*
 #define MAX_LOADSTRING 100
 
 // Zmienne globalne:
@@ -182,3 +267,6 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     }
     return (INT_PTR)FALSE;
 }
+
+*/
+
